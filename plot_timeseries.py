06729cd77b23bc43e24d376cd13f7897f779472e
SeanStafford <39390590+SeanStafford@users.noreply.github.com>
2018-05-26 12:50:55 -0400

    Fixed bug with python script
    If TimeSeries.txt had a single line the python script would crash. Loadtxt needed "ndmin=2"

--- plot_timeseries.py
+++ plot_timeseries.py
@@ -3,7 +3,7 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
-data = np.loadtxt("TimeSeries.txt", delimiter=' ')
+data = np.loadtxt("TimeSeries.txt", delimiter=' ', ndmin=2)
 plt.plot(data[:,0], data[:,1], '-')
 plt.savefig("TimeSeries.png")
 plt.clf()
